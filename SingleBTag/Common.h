03c6af2a3b584efedda92d274deaec2cb6d363d6
Aliaksei Raspiareza <rasp@mail.desy.de>
2020-09-29 01:48:20 +0200

    header and styles files

---
+++ SingleBTag/Common.h
@@ -0,0 +1,16 @@
+
+TString cuts[5] = {"bdtout<0.0",
+		   "bdtout>=0.0&&bdtout<0.3",
+		   "bdtout>=0.3&&bdtout<0.6",
+		   "bdtout>=0.6&&bdtout<0.8",
+		   "bdtout>=0.8"};
+  
+TString names[5] = {"CAT4",
+		    "CAT5",
+		    "CAT6",
+		    "CAT7",
+		    "CAT8"};
+
+int Nbins = 1200;
+float xmin = 80;
+float xmax = 200;
