eb3d036100f90f987fe26488408933b607019428
Aliaksei Raspiareza <rasp@mail.desy.de>
2020-09-29 09:49:10 +0200

    updated scripts

--- CombineCards.sh
+++ CombineCards.sh
@@ -3,5 +3,6 @@
 cp SingleBTag/signal_singleb_shapes.root ./
 cp SingleBTag/data_singleb_shapes.root ./
 cp SingleBTag/datacards_singleb_*.txt ./
+rm datacards_singleb.txt
 combineCards.py datacards_singleb_CAT4.txt datacards_singleb_CAT5.txt datacards_singleb_CAT6.txt datacards_singleb_CAT7.txt datacards_singleb_CAT8.txt > datacards_singleb.txt
 #combineCards.py datacards_doubleb.txt datacards_singleb.txt > datacards_all.txt
