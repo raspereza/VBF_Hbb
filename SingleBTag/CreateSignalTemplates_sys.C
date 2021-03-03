8e7dedc94249fcef5c6512f9f4498ab88af1f841
mukherje <soumya.mukherje@lxplus.cern.ch>
2021-03-03 22:17:40 +0100

    JEC-JER inclusion in signal shape CB function

---
+++ SingleBTag/CreateSignalTemplates_sys.C
@@ -0,0 +1,109 @@
+#include "Common.h"
+
+
+TString DN_JEC[] = {"14.6745","-2.4750","24.2355","16.1985","11.8725"};
+TString DN_JER[] = {"0.7410","2.2470","-0.0875","-2.2070","0.3160"};
+
+TString Dmean_JEC[] = {"1.0950","3.1400","0.7640","1.6165","0.6395"};
+TString DSigma_JER[] = {"0.5252","2.55105","0.1796","0.7321","0.2859"};
+
+using namespace RooFit;
+
+void CreateSignalPDF(int iCAT,
+		     TNtuple * treeGGH,
+		     TNtuple * treeVBF,
+		     RooWorkspace * w) {
+
+  TH1::SetDefaultSumw2(true);
+
+  TString nameGGHHist = "mbb_ggh"+names[iCAT];
+  TString nameVBFHist = "mbb_vbf"+names[iCAT];
+  TH1D * histGGH = new TH1D(nameGGHHist,"",NbinsSig,xmin,xmax);
+  TH1D * histVBF = new TH1D(nameVBFHist,"",NbinsSig,xmin,xmax);
+
+  TCanvas * dummy = new TCanvas("dummy","",800,700);
+  treeGGH->Draw("mbb>>"+nameGGHHist,"weight*("+cuts[iCAT]+")");
+  treeVBF->Draw("mbb>>"+nameVBFHist,"weight*("+cuts[iCAT]+")");
+  delete dummy;
+
+  Float_t yieldGGH = histGGH->GetSumOfWeights();
+  Float_t yieldVBF = histVBF->GetSumOfWeights();
+  RooRealVar yield_GGH("ggh_yield_"+names[iCAT],"Yield",yieldGGH,0.,2*yieldGGH);
+  RooRealVar yield_VBF("vbf_yield_"+names[iCAT],"Yield",yieldVBF,0.,2*yieldVBF);
+
+  RooRealVar mbb("mbb_"+names[iCAT],"mass(bb)",xmin,xmax);
+
+  RooRealVar mean("mean_sig_"+names[iCAT],"Mean",125,80,200);
+  RooRealVar sigma("sigma_sig_"+names[iCAT],"Width",10,0,30);
+  RooRealVar alpha("alpha_sig_"+names[iCAT],"Alpha",0.5,0,10.);
+  RooRealVar n("n_sig_"+names[iCAT],"n",10,0,20);
+    
+  RooRealVar b0("b0_sig_"+names[iCAT],"b0",0,1);
+  RooRealVar b1("b1_sig_"+names[iCAT],"b1",0,1);
+  RooRealVar b2("b2_sig_"+names[iCAT],"b2",0,1);
+  b0.setConstant(false);
+  b1.setConstant(false);
+  b2.setConstant(false);
+
+  RooRealVar fsig("fsig_"+names[iCAT],"fsig",0,1);
+
+  RooRealVar theta_JES("CMS_vbfbb_scale_mbb_13TeV_2018","Mbb_scale",1.0,0.5,1.5);
+  RooRealVar theta_JER("CMS_vbfbb_res_mbb_13TeV_2018","Mbb_scale",1.0,0.5,1.5);
+    
+  //RooFormulaVar yield_shifted("yield_shifted_sig_"+names[iCAT],"@0  + " + DN_JEC[iCAT] + "*@1 +"   + DN_JER[iCAT] +  "  *@2",RooArgList(yield_VBF,theta_JES,theta_JER));
+  RooFormulaVar mean_shifted ("mean_shifted_sig_"+names[iCAT],"@0  + " + Dmean_JEC[iCAT] + "*@1",RooArgList(mean,theta_JES));
+  RooFormulaVar sigma_shifted("sigma_res_sig_"+names[iCAT],"@0  + " + DSigma_JER[iCAT] + "*@1",RooArgList(sigma,theta_JER));
+
+  RooBernstein BRN("brn_sig_"+names[iCAT],"Bernstein",mbb,RooArgList(b0,b1,b2));
+  RooCBShape cbx("cb_sig_"+names[iCAT],"CBshape",mbb,mean,sigma,alpha,n);
+  RooAddPdf signalx("signalx","signal",RooArgList(cbx,BRN),fsig);
+
+  histVBF->Add(histGGH,histVBF);
+  RooDataHist data("dataGGH","dataGGH",mbb,histVBF);
+  RooFitResult * res = signalx.fitTo(data,Save());
+
+  RooCBShape cb("cb_sig_"+names[iCAT],"CBshape",mbb,mean_shifted,sigma_shifted,alpha,n);
+  RooAddPdf signal("sig_"+names[iCAT],"signal",RooArgList(cb,BRN),fsig);
+
+  alpha.setConstant(true);
+  mean.setConstant(true);
+  sigma.setConstant(true);
+  n.setConstant(true);
+  fsig.setConstant(true);
+  b0.setConstant(true);
+  b1.setConstant(true);
+  b2.setConstant(true);
+
+  cout << endl;
+  cout << "+++++++++++++ " << names[iCAT] << "++++++++++ " << endl;
+  cout << endl;
+  b0.Print();
+  b1.Print();
+  b2.Print();
+  cout << "yields : ggH = " << yieldGGH 
+       << "   qqH = " << yieldVBF
+       << "   total = " << yieldGGH+yieldVBF << std::endl;
+  res->Print();
+
+  w->import(signal);
+  w->import(yield_GGH);
+  w->import(yield_VBF);
+}
+
+void CreateSignalTemplates_sys() {
+  TFile * file = new TFile("/afs/cern.ch/user/t/tumasyan/public/For_Soumya/Systematics_ROOTS/mbb_and_bdt_all_Nom_JEC.root");
+  
+  TNtuple * treeGGH = (TNtuple*)file->Get("Mass_and_BDT_ggF_Hbb");
+  TNtuple * treeVBF = (TNtuple*)file->Get("Mass_and_BDT_VBF_Hbb");
+
+  TFile * fileOutput = new TFile("root_shapes_sys/signal_singleb_shapes.root","recreate");
+  fileOutput->cd("");
+  RooWorkspace * w = new RooWorkspace("w","signal");
+
+  for (int i=0; i<5; ++i) 
+    CreateSignalPDF(i,treeGGH,treeVBF,w);
+
+  w->Write("w");
+  fileOutput->Write();
+  fileOutput->Close();
+}
