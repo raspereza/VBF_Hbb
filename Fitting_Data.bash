78f5bcef2a69ff54e808a2edc1cc1f24d0ee9a2a
Aliaksei Raspiareza <rasp@mail.desy.de>
2021-12-10 12:09:53 +0100

    fitting procedure

--- Fitting_Data.bash
+++ Fitting_Data.bash
@@ -2,4 +2,4 @@
 # $1 - suffix
 datacard=$1 
 #combine -M FitDiagnostics  --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND --X-rtd ADDNLL_RECURSIVE=0 --X-rtd FITTER_NEW_CROSSING_ALGO --expectSignal=1 --robustFit 1 --rMin=-20 --rMax=20 -m 125 ${datacard}.txt --cminDefaultMinimizerTolerance 0.01 --cminDefaultMinimizerStrategy=0 --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL -v2 
-combine -M FitDiagnostics --robustHesse 1 --cminDefaultMinimizerTolerance 0.01 --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy=0 -m 125  -v 5
+combine -M FitDiagnostics --robustHesse 1 --cminDefaultMinimizerTolerance 0.01 --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy=0 --X-rtd FITTER_NEW_CROSSING_ALGO --rMin=-20 --rMax=20 -m 125 ${1}.txt -v 5 
