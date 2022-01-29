495502dbe51efe4a0b1d15368e2c31ebd60e567f
Aliaksei Raspiareza <rasp@mail.desy.de>
2022-01-29 17:15:53 +0100

    UL 2016 updated

---
+++ SingleBTag_2016_UL_v2/CreateDataTemplates_chb.C
@@ -0,0 +1,116 @@
+#include "Common.h"
+
+using namespace RooFit;
+
+void CreatePDF (int iCAT,
+		int iORDER,
+		TTree * tree,
+                TNtuple * tree_zj,
+                TNtuple * tree_vbf,
+                TNtuple * tree_ggh,
+		RooWorkspace * w) {
+
+  TH1::SetDefaultSumw2(true);
+
+  TCanvas * dummy = new TCanvas("dummy","",800,700);
+  TString nameHist = "mbb_"+names[iCAT];
+  TString nameHist_zj = "mbb_zj_"+names[iCAT];
+  TString nameHist_vbf = "mbb_vbf_"+names[iCAT];
+  TString nameHist_ggh = "mbb_ggh_"+names[iCAT];
+
+  TH1D * hist = new TH1D(nameHist,"",Nbins,xmin,xmax);
+  TH1D * hist_zj = new TH1D(nameHist_zj,"",Nbins,xmin,xmax);
+  TH1D * hist_vbf = new TH1D(nameHist_vbf,"",Nbins,xmin,xmax);
+  TH1D * hist_ggh = new TH1D(nameHist_ggh,"",Nbins,xmin,xmax);
+
+  tree->Draw(variable+">>"+nameHist,cuts[iCAT]);
+  tree_zj->Draw(variable+">>"+nameHist_zj,"weight*("+cuts[iCAT]+")");
+  tree_vbf->Draw(variable+">>"+nameHist_vbf,"weight*("+cuts[iCAT]+")");
+  tree_ggh->Draw(variable+">>"+nameHist_ggh,"weight*("+cuts[iCAT]+")");
+
+  TH1D * histData = (TH1D*)hist->Clone("dataHist_"+names[iCAT]);
+
+  hist->Add(hist_zj,-znorm[iCAT]);
+  hist->Add(hist_vbf,-1);
+  hist->Add(hist_ggh,-1);
+
+  delete dummy;
+
+  RooRealVar mbb("mbb_"+names[iCAT],"mass(bb)",xmin,xmax);
+  RooRealVar b0("b0_"+names[iCAT],"b0",-1.0,1.0);
+  RooRealVar b1("b1_"+names[iCAT],"b1",-1.0,1.0);
+  RooRealVar b2("b2_"+names[iCAT],"b2",-1.0,1.0);
+  RooRealVar b3("b3_"+names[iCAT],"b3",-1.0,1.0);
+  RooRealVar b4("b4_"+names[iCAT],"b4",-1.0,1.0);
+  RooRealVar b5("b5_"+names[iCAT],"b5",-1.0,1.0);
+  b0.setConstant(false);
+  b1.setConstant(false);
+  b2.setConstant(false);
+  b3.setConstant(false);
+  b4.setConstant(false);
+  b5.setConstant(false);
+
+  Float_t yield = hist->GetSumOfWeights();
+  RooRealVar qcd_yield("qcd_"+names[iCAT]+"_norm","Yield",yield,0.5*yield,2*yield);
+
+  RooArgList argList(b0,b1);
+  if (iORDER>=3)
+    argList.add(b2);
+  if (iORDER>=4)
+    argList.add(b3);
+  if (iORDER>=5)
+    argList.add(b4);
+  if (iORDER>=6)
+    argList.add(b5);
+  
+  Roo
+
+  //  RooChebychev CHV("qcd_"+names[iCAT],"qcd_"+names[iCAT],mbb,argList);
+  RooExponential CHV("qcd_"+names[iCAT],"qcd_"+names[iCAT],);
+  RooDataHist dataSubtr("data_subtr_"+names[iCAT],"data",mbb,hist);
+  RooDataHist data("data_"+names[iCAT],"data",mbb,histData);  
+
+  RooFitResult * res = CHV.fitTo(dataSubtr,Save(),SumW2Error(kTRUE));  
+
+  cout << endl;
+  cout << "+++++++++++++++ " << names[iCAT] << " +++++++++++++++++++" << endl;
+  cout << endl;
+  b0.Print();
+  b1.Print();
+  b2.Print();
+  b3.Print();
+  b4.Print();
+  b5.Print();
+  res->Print();
+  cout << "TOTAL YIELD = " << yield << endl;
+  cout << endl;
+  cout << "+++++++++++++++++++++++++++++++++++++++++" << endl;
+  cout << endl;
+
+  w->import(CHV);
+  w->import(data);
+  w->import(qcd_yield);
+
+}
+
+void CreateDataTemplates_chb() {
+
+
+  TFile * file = new TFile(dirName+"/mbb_and_bdt_all_Nom.root");
+  TTree * tree = (TTree*)file->Get("Mass_and_BDT_DATA");
+  TNtuple * tree_zj = (TNtuple*)file->Get("Mass_and_BDT_ZJets");
+  TNtuple * tree_vbf = (TNtuple*)file->Get("Mass_and_BDT_VBF_Hbb_Dipol");
+  TNtuple * tree_ggh = (TNtuple*)file->Get("Mass_and_BDT_ggF_Hbb_POWHEG");
+ 
+  TFile * fileOutput = new TFile("root_shape/data_singleb_shapes.root","recreate");
+  fileOutput->cd("");
+  RooWorkspace * w = new RooWorkspace("w","data");
+
+  int iorder[5] = {5,2,2}; 
+  for (int i=0; i<3; ++i) 
+    CreatePDF(i,iorder[i],tree,tree_zj,tree_vbf,tree_ggh,w);
+  
+  w->Write("w");
+  fileOutput->Write();
+  fileOutput->Close();
+}
