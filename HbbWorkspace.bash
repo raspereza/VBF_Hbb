0bac0d46fb40acd1cdb2afa0305a2bbad39be81b
Aliaksei Raspiareza <rasp@mail.desy.de>
2022-09-23 15:39:07 +0200

    creating workspace with 3POIs

--- HbbWorkspace.bash
+++ HbbWorkspace.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-text2workspace.py ${1}.txt -m 125.0 -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO 'map=.*/qqH_hbb:r_qqH[1,-20,20]' --PO 'map=.*/ggH_hbb:r_ggH[1,-30,30]' -o ${1}.root
+text2workspace.py ${1}.txt -m 125.0 -P HiggsAnalysis.CombinedLimit.PhysicsModel:multiSignalModel --PO 'map=.*/qqH_hbb:r_qqH[1,-30,30]' --PO 'map=.*/ggH_hbb:r_ggH[1,-30,30]' -o ${1}_3POIs.root
