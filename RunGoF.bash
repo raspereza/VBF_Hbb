77362ebab3420f996d72a64797052727eff20318
Aliaksei Raspiareza <rasp@mail.desy.de>
2022-09-24 21:21:12 +0200

    Running goodness of fit test

---
+++ RunGoF.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+# $1 - datacards w/o 
+# $2 - algo (saturated, KS, AD)
+# $3 - number of toys (typically >1000)
+ulimit -s unlimited
+ws=$1
+algo=$2
+ntoys=$3
+combine -M GoodnessOfFit -d ${ws}.txt -m 125 --algo ${algo} -n _obs.${ws}
+combine -M GoodnessOfFit -d ${ws}.txt -m 125 -t ${ntoys} --toysFreq --algo ${algo} -n _exp.${ws}
