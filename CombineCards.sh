5598f2bba06d07ecb819cd9598faf1b80c6ae9e3
Aliaksei Raspiareza <rasp@mail.desy.de>
2022-09-24 21:09:15 +0200

    removed obsolete scripts

--- CombineCards.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm *.txt
-./CombineCards_DoubleB_2016_UL.sh
-./CombineCards_DoubleB_2018_UL_v2.sh
-./CombineCards_SingleB_2016_UL_v3.sh
-./CombineCards_SingleB_2018_UL_v2.sh
-combineCards.py datacards_doubleb_2016_UL.txt datacards_doubleb_2018_UL_v2.txt datacards_singleb_2016_UL_v3.txt datacards_singleb_2018_UL_v2.txt > datacards_Run2_UL.txt
-
