38dda27b65c5b6ee829613310f42fd22b51d2777
Aliaksei Raspiareza <rasp@mail.desy.de>
2022-09-24 21:08:01 +0200

    removed obsolete scripts

--- Fitting_Data_ws.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# $1 - datacards w/o root extension
-combine -M FitDiagnostics --robustFit 1 --cminDefaultMinimizerTolerance 0.01 --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy=0 --X-rtd FITTER_NEW_CROSSING_ALGO --redefineSignalPOIs r_ggH,r_qqH -m 125 -d ${1}.root -v 5 
