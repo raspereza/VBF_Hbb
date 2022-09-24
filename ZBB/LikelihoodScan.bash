2a39b5c09f3ea4207be695f8e4a5da548e8c6f13
Aliaksei Raspiareza <rasp@mail.desy.de>
2022-09-24 21:27:18 +0200

    removing old stuff

--- ZBB/LikelihoodScan.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#ulimit -s unlimited
-#combineTool.py -m 91 -M MultiDimFit --setParameters r=1 --setParameterRanges r=0.8,1.8 --points 51 --redefineSignalPOIs r -d $1.root --algo grid -n .r_obs --alignEdges 1 --cminDefaultMinimizerStrategy=0 
-
-python scripts/plot1DScan.py --main=higgsCombine.r_obs.MultiDimFit.mH91.root --POI=r --output=r --no-numbers --no-box --x_title="#mu_{Zbb}" --x-min=0.6 --x-max=2.0 --y-max=2.0 --main-label="#mu_{Zbb} "
-
--- ZBB/RunFit_Data.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# $1 - RooT file with workspace (w/o extension root)
-datacard=$1 
-combine -M FitDiagnostics  --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND --X-rtd ADDNLL_RECURSIVE=0 --setParameters r=1 --redefineSignalPOIs r --X-rtd FITTER_NEW_CROSSING_ALGO --robustFit 1 --rMin=-20 --rMax=20 -m 125 -d ${datacard}.root --cminDefaultMinimizerTolerance 0.01 --cminDefaultMinimizerStrategy=0 --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL -v2 
-mv fitDiagnostics.root mlfit_obs.root
