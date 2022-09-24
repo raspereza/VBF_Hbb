23c1add9da35237f3cb42f424dbc847bfcfda45a
Aliaksei Raspiareza <rasp@mail.desy.de>
2022-09-24 21:06:11 +0200

    removed obsolete scripts

--- CombineCards_singleb_ZBB.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-rm -rf datacards_singleb_ZBB.txt datacards_singleb_CAT*ZBB.txt
-cp ZBB_SingleB/datacards/datacards_singleb_CAT*ZBB.txt ./
-combineCards.py datacards_singleb_CAT*ZBB.txt > datacards_singleb_ZBB.txt
