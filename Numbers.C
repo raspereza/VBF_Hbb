de438c815f7659c2fe1ddb07cda623c9e5fe2446
Aliaksei Raspiareza <rasp@mail.desy.de>
2022-10-10 16:27:43 +0200

    code to compute event yields

---
+++ Numbers.C
@@ -0,0 +1,49 @@
+
+void Numbers(TString folder = "DoubleBTag_2016_UL_Preapproval_kNLO_v2") {
+
+  TString variableData = "mbb_reg";
+  TString variable = "mbb_reg_smr";
+
+  std::map<TString, std::vector<TString> > samples = {
+    {"ggH",{"Mass_and_BDT_ggF_Hbb_POWHEG"}},
+    {"qqH",{"Mass_and_BDT_VBF_Hbb_Dipol"}},
+    {"DY",{"Mass_and_BDT_ZJets","Mass_and_BDT_ZJets_EWK"}},
+    {"Top",{"Mass_and_BDT_Sandgle_Top","Mass_and_BDT_tt"}},
+    {"W",{"Mass_and_BDT_WJets"}},
+    {"QCD",{"Mass_and_BDT_QCD"}},
+    {"Data",{"Mass_and_BDT_DATA"}}
+  };
+
+  TFile * file = new TFile(folder+"/ntuples/mbb_and_bdt_all_Nom.root");
+  for (auto sample : samples) {
+    TString name = sample.first;
+    std::vector<TString> sampleNames = sample.second;
+    TH1D * hist = new TH1D("hist_"+name,"",60,80,200);
+    for (auto sampleName : sampleNames) {
+      TNtuple * tuple = (TNtuple*)file->Get(sampleName);
+      TString histName = sampleName + "_hist";
+      //      std::cout << histName << std::endl;
+      TH1D * histSample = new TH1D(histName,"",60,80,200);
+      if (name=="Data")
+	tuple->Draw(variableData+">>"+histName);
+      else if (name=="W"||name=="Top")
+	tuple->Draw(variableData+">>"+histName,"weight");
+      else
+	tuple->Draw(variable+">>"+histName,"weight");
+      hist->Add(hist,histSample,1.,1.);
+    }
+    double err2 = 0;
+    double yield = 0;
+    for (int iB=1; iB<=60; ++iB) {
+      double x = hist->GetBinContent(iB);
+      double err = hist->GetBinError(iB);
+      yield += x;
+      err2 += err*err;
+    }
+    double yieldE = TMath::Sqrt(err2);
+    std::cout << name << " : " << yield << " +/- " << yieldE << std::endl;
+
+  }
+
+
+}
