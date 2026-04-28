ad002ee34738f3f2281515a785a05ccf63d9e174
Alexei Raspereza <rasp@mail.desy.de>
2026-04-28 19:09:11 +0200

    update Significance_Data.bash

--- Significance_Data.bash
+++ Significance_Data.bash
@@ -1,6 +1,13 @@
 #!/bin/bash
-# $1 - suffix
-# $2 - signal strength
-datacards=$1 
+# $1 - datacard name w/o extension *.txt
+n=$#
+if [[ $n -ne 1 ]]; then
+    echo usage : Significance_Data.bash [DATACARD_NAME]
+    echo DATACARD_NAME = [datacards_Run2, datacards_Run3, datacards_Run2_Run3]
+    exit
+fi
+
+datacards=$1
+cd datacards_Run2_Run3
 combine -M Significance --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND --X-rtd ADDNLL_RECURSIVE=0 --X-rtd FITTER_NEW_CROSSING_ALGO --expectSignal=0 -m 125 ${datacards}.txt
-#combineTool.py -M Significance --cminDefaultMinimizerTolerance 0.01 --X-rtd MINIMIZER_analytic --cminDefaultMinimizerStrategy=0 --X-rtd FITTER_NEW_CROSSING_ALGO --rMin=-3 --rMax=3 -m 125 ${1}.txt
+cd -
